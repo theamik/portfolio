f6ea9dc3b20ae7844e40059b900ed2a7709e1a80
theamik <amik.sylhet@gmail.com>
2024-03-10 10:41:47 +0600

    update

---
+++ src/data/ProjectData.js
@@ -0,0 +1,137 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/ykte-feature-graphic.png",
+    title: "E-Commerce Site",
+    description:
+      "A e-commerce website where customer show their products as well as generate sales.",
+    tech_stack: ["React Js", "Express Js", "Node Js", "TailwindCSS"],
+    github_url: "",
+    demo_url: "https://bashundhara.vercel.app/",
+  },
+  {
+    img: "/renr-responsive.png",
+    title: "Course Bundler App",
+    description:
+      "A learning application for the students where after subscribe get bundle of courses and lectures",
+    tech_stack: ["React", "Chakra Ui", "Node Js"],
+    github_url: "",
+    demo_url: "https://coursebundler-two.vercel.app/",
+  },
+  {
+    img: "/mymind-responsive.png",
+    title: "Retail Master",
+    description:
+      "An app where retailer can manage his shop maintain purchases,sales,accounts.",
+    tech_stack: ["HTML", "CSS", "NodeJs", "MongoDB", "SocketIo"],
+    github_url: "https://github.com/salesp07/MyMind---Copy",
+    demo_url: "https://retail-master-virid.vercel.app/admin/login",
+  },
+  {
+    img: "/pokedex-responsive.png",
+    title: "Multi-Vendor Site",
+    description:
+      "A Multi-Vendor Mobile Responsive E-commerce site. Lots of sellers show their products and customers purchase from it and they can chat with each-others",
+    tech_stack: ["ReactJs", "NodeJs", "MongoDB", "SocketIo"],
+    github_url: "https://github.com/salesp07/Pokedex-ReactApp",
+    demo_url: "https://laptopworkshop.vercel.app/",
+  },
+  {
+    img: "/jobDetect.png",
+    title: "Restaurant Management",
+    description:
+      "A mobile responsive app to manage restaurant orders, inventories, expenses and income , generate reports.",
+    tech_stack: ["ReactJs", "NodeJs", "MongoDB", "TailwindCSS"],
+    github_url: "https://github.com/salesp07/JobDetectNode",
+    demo_url: "https://soam-food.vercel.app/",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg",
+    name: "NextJs",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    name: "Typescript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "React Native",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain.svg",
+    name: "Firebase",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Python",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flask/flask-original.svg",
+    name: "Flask",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    name: "C",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+    name: "Java",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+    name: "MySQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+    name: "Material UI",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
+    name: "Figma",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-plain.svg",
+    name: "TailwindCSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
